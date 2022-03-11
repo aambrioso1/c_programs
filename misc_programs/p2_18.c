b3a3e63068d7cee3619ab2017bc4051050c3d276
Alejandro M <alejandro.moncayo@gmail.com>
2022-03-11 17:39:39 -0500

    Update p2_18.c
    adding the contents for p2_18.c

--- misc_programs/p2_18.c
+++ misc_programs/p2_18.c
@@ -6,16 +6,36 @@
 void get_extension(char *file_name, char *extension);
 
 int main() {
-
-
-
+    //File name: "Go ahead and make my.day"
+    //expected extension: "day"
+    char fileName[] = {'G','o',' ','a','h','e','a','d',' ','m','a','k','e',' ','m','y','.','d','a','y','\0'};
+    char ext[10] = {'\0'};
+    get_extension(fileName,ext);
+    printf("%s\n",ext);
     return 0;
 }
 
 
+void get_extension(char *file_name, char *extension) {
+    char *p;
+
+    //At the end of this loop *p = '.' or *p = '\0'
+    for (p= file_name; *p!= '\0'; p++)
+        if(*p == '.')
+            break;
+    
+    //Only run next loop if *p == '.'
+    if(*p != '\0'){
+        for(p=p+1; *p != '\0'; p++){
+            *extension++ = *p; //assing *extension the value of *p and move to the next character
+        }
+    }
+
+    //Assing '\0' to the extension
+    *extension = '\0';
+}
 
 /********************* OUTPUT **************************
-
-
-
+File name: "Go ahead and make my.day"
+expected extension: "day"
 ********************************************************/
