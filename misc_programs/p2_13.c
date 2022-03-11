f608bde757a320a05c98b0ab3272ce40df958a3b
aambrioso1 <aambrioso1@gmail.com>
2022-03-11 12:42:54 -0500

    Add problem 13

--- misc_programs/p2_13.c
+++ misc_programs/p2_13.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
-#include <string.h>
 
 /***** Problem 13 on Practice Test 2 *****/
 
-
+int read_line(char *str, int n) {
+    int ch, i = 0;
+    while((ch = getchar()) == ' ');  // Skips initial white space.
+    *str++= ch;
+    i++;
+    while ((ch = getchar()) != '\n') { 
+        if (i < n) {
+            *str++= ch;
+            i++;
+        }
+    }
+    *str = '\0'; 
+    return i; 
+}
 
 int main(void) {
+
+    char s[101];    
+    read_line(s, 101);
+    printf("The string without initial white spaces is \"%s.\"", s);
 
     return 0;
 }
@@ -14,6 +30,7 @@ int main(void) {
 
 /********************* OUTPUT **************************
 
-
+Input:         I like   computer     science
+Output: The string without initial white spaces is "I like   computer     science."
 
 ********************************************************/
