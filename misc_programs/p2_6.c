0c5ee46ee5ad484b775a5bb0648e74bc04d25b3e
aambrioso1 <aambrioso1@gmail.com>
2022-03-12 00:52:18 -0500

    Add an example to p2.6.c

--- misc_programs/p2_6.c
+++ misc_programs/p2_6.c
@@ -4,12 +4,21 @@ int main() {
     
     char *planets[] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
 
-    // For string arrays a[row][column] the row indicates the string and the column a letter within the string.
+    // For string array a[row][column] the row indicates the string and the column a letter within the string.
     // As usual indexing starts at 0.
-    printf("planets[1][3] = %c", planets[1][3]);
+    printf("planets[1][3] = %c\n", planets[1][3]);
+
+    // Shows that planets = &planets[0] where "Mercury is stored"
+    printf("*planets = %s\n", *planets);
+    printf("*(planets + 3) = %s\n", *(planets+3));
 
     return 0;
-
 }
 
-// Output: planets[1][3] = u
+/******** Output *********
+
+planets[1][3] = u
+*planets = Mercury
+*(planets + 3) = Mars
+
+**************************/
